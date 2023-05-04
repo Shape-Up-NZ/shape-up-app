3a4e5793672930f0fd0a9f9284ad37867367664c
travyozza <84102421+travyozza@users.noreply.github.com>
2023-05-04 17:54:35 +1200

    Image formatting

--- shape-up/src/components/Navbar.js
+++ shape-up/src/components/Navbar.js
@@ -17,7 +17,7 @@ const Navbar = () => {
     <Stack direction="row" justifyContent="space-around" sx={{gap: {sm: '122px', xs: '40px'}, mt: {sm: '32px', xs: '20px'}, justifyContent: 'none'}} px="20px">
       <Link to="/">
         <img src={Logo} alt="logo" style={{
-          width: '120px', height: '120px', margin: '0px 20px'}} />
+          width: '94px', height: '83px', margin: '10px 2px 0 0'}} />
       </Link>
       <Stack
         direction="row"
