98e7b617334a092de3ec49531ec7f0d030fae42b
travyozza <84102421+travyozza@users.noreply.github.com>
2023-05-04 15:32:05 +1200

    folder management

--- shape-up/src/components/HeroBanner.js
+++ shape-up/src/components/HeroBanner.js
@@ -12,7 +12,7 @@ const HeroBanner = () => {
         <Typography fontWeight={700} sx={{fontSize: {lg: '44px', xs: '40px'}}}>
             We are here to help <br /> you to achieve your <br /> fitness dreams.
         </Typography>
-        <Button variant="contained" color="success" href="#exercises">What we offer</Button>
+        <Button variant="contained" color="success" href="../pages/features">What we offer</Button>
         <img src={HeroBannerImage} alt="banner" className="hero-banner-img"/>
     </Box>
   )
