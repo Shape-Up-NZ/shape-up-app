d47382cbc2e51e2bfe27c8fa49f4f376fd859516
MiguelEmmara-ai <m.miguelemmara@gmail.com>
2023-05-26 23:41:52 +1200

    Async Handler

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,10 @@
+import asyncHandler from "express-async-handler";
+
 // @desc        Auth user/set token
 // route        POST /api/users/auth
 // @access      Public
-const authUser = (req, res) => {
-    res.status(200).json({ message: "Auth User" });
-  };
-  
-  export { authUser };
+const authUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Auth User" });
+});
+
+export { authUser };
