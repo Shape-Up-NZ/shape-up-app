16bfa2cd3ad4aecdbd28b21c28f8ea6b2de11b2d
MiguelEmmara-ai <m.miguelemmara@gmail.com>
2023-05-27 15:33:28 +1200

    Add Login Page

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    // Perform login logic here, such as making an API request to authenticate the user
+    // You can use the 'email' and 'password' state variables for this purpose
+    // After successful login, you can redirect the user to another page or update the UI accordingly
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={handleEmailChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
