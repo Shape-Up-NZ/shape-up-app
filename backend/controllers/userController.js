39e48ba05c0275a7a6eb703cb1419c8f969aa8b5
MiguelEmmara-ai <m.miguelemmara@gmail.com>
2023-05-27 14:30:12 +1200

    (Template) User Controller Functions, Basic Auth Function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,4 +7,38 @@ const authUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Auth User" });
 });
 
-export { authUser };
+// @desc        Register a new user
+// route        POST /api/users
+// @access      Public
+const registerUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Register User" });
+});
+
+// @desc        Logout a new user
+// route        POST /api/users/logout
+// @access      Public
+const logoutUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Logout User" });
+});
+
+// @desc        Get user profile
+// route        GET /api/users/profile
+// @access      Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "User Profile" });
+});
+
+// @desc        Update user profile
+// route        PUT /api/users/profile
+// @access      Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Update User Profile" });
+});
+
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+};
