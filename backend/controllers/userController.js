cbb60f1626fe8b7f5347fe3102c3907106e913d4
MiguelEmmara-ai <m.miguelemmara@gmail.com>
2023-05-27 15:07:49 +1200

    Update User Profile Endpoint
    User Can Now Edit Their Profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -84,7 +84,27 @@ const getUserProfile = asyncHandler(async (req, res) => {
 // route        PUT /api/users/profile
 // @access      Private
 const updateUserProfile = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Update User Profile" });
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+
+    res.status(200).json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User Not Found");
+  }
 });
 
 export {
