39bfb7aeaaebbb1223e724c7c0eb72fd8c924004
MiguelEmmara-ai <m.miguelemmara@gmail.com>
2023-05-27 14:52:26 +1200

    Auth User Endpoint User Can Now Register And Login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,7 +6,21 @@ import User from "../models/userModel.js";
 // route        POST /api/users/auth
 // @access      Public
 const authUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Auth User" });
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid user credentials");
+  }
 });
 
 // @desc        Register a new user
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -22,7 +22,7 @@ const userSchema = mongoose.Schema(
   }
 );
 
-// Hash The Password When User Register
+// Hash The Password
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) {
     next();
@@ -31,6 +31,11 @@ userSchema.pre("save", async function (next) {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
+
+// Compare Password When User Login
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 const User = mongoose.model("User", userSchema);
 
