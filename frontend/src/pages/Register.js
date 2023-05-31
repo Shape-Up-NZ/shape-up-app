88fc655a5bd52c2702dacebe8ba03c9694a9ea5c
MiguelEmmara-ai <m.miguelemmara@gmail.com>
2023-05-31 22:14:07 +1200

    Register Functionality

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,9 +1,14 @@
 import React from "react";
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { Container, Form, Button, Row, Col } from "react-bootstrap";
 import { BsEnvelope, BsLock } from "react-icons/bs";
+import { useDispatch, useSelector } from "react-redux";
 import FormContainer from "../components/FormContainer";
+import { useRegisterMutation } from "../slices/usersApiSlice";
+import { setCredentials } from "../slices/authSlice";
+import { toast } from "react-toastify";
+import Loader from "../components/Loader";
 
 const Register = () => {
   const [name, setName] = useState("");
@@ -11,9 +16,33 @@ const Register = () => {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
 
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [register, { isLoading }] = useRegisterMutation();
+
+  const { userInfo } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (userInfo) {
+      navigate("/");
+    }
+  }, [navigate, userInfo]);
+
   const submitHandler = async (e) => {
     e.preventDefault();
-    console.log("Submit");
+    if (password !== confirmPassword) {
+      toast.error("Password do not match");
+    } else {
+      try {
+        const res = await register({ name, email, password }).unwrap();
+        dispatch(setCredentials({ ...res }));
+        navigate("/");
+        toast.success("Register Successfully!");
+      } catch (err) {
+        toast.error(err.data.message || err.error);
+      }
+    }
   };
 
   return (
@@ -47,9 +76,21 @@ const Register = () => {
               type="password"
               placeholder="Enter Password"
               value={password}
-              onChange={(e) => setEmail(e.target.value)}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </Form.Group>
+
+          <Form.Group controlId="confirmPassword" className="mb-2">
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter Confirm Password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+
+          {isLoading && <Loader />}
 
           <Button variant="primary" type="submit">
             <BsLock /> Sign Up
@@ -60,7 +101,6 @@ const Register = () => {
               Already Have An Account? <Link to="/pages/login">Login</Link>
             </Col>
           </Row>
-          
         </Form>
       </FormContainer>
     </div>
