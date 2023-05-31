f2bf905bb37d6edc3a994f739f887ca7d0d5a20d
MiguelEmmara-ai <m.miguelemmara@gmail.com>
2023-05-31 21:58:43 +1200

    Header (Navbar) Revamp

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ import "react-toastify/dist/ReactToastify.css";
 
 import "./App.css";
 import Home from "./pages/Home";
-import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 import Features from "./pages/Features";
 import Workouts from "./pages/Workouts";
 import Bmrcalculator from "./pages/bmrcalculator";
@@ -20,7 +20,7 @@ const App = () => {
     <Container>
       <ToastContainer />
       <Box width="400px" sx={{ width: { x1: "1488px" } }} m="auto">
-        <Navbar />
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/pages/features" element={<Features />} />
