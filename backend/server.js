8bf317bcbb663f96051a0652b258c35a7dc5d9a1
MiguelEmmara-ai <m.miguelemmara@gmail.com>
2023-06-07 18:36:29 +1200

    production rename

--- backend/server.js
+++ backend/server.js
@@ -25,10 +25,10 @@ app.use("/api/user", UserMealPlanRoutes);
 
 if (process.env.NODE_ENV === "production") {
     const __dirname = path.resolve();
-    app.use(express.static(path.join(__dirname, "frontend/build")));
+    app.use(express.static(path.join(__dirname, "frontend/dist")));
   
     app.get("*", (req, res) =>
-      res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+      res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
     );
   } else {
     app.get("/", (req, res) => res.send("Server is ready"));
